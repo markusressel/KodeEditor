40d5ca18f0b60e3c8f2c5b29bc745a9e90e727fd
Markus Ressel <markus.ressel@berlin.de>
2018-08-27 16:15:20 +0200

    added "about libraries" string resources added some theming attributes updated compileSdk v0.5.0

--- library/src/main/java/de/markusressel/kodeeditor/library/view/CodeEditorView.kt
+++ library/src/main/java/de/markusressel/kodeeditor/library/view/CodeEditorView.kt
@@ -1,12 +1,18 @@
 package de.markusressel.kodeeditor.library.view
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
+import android.support.annotation.AttrRes
+import android.support.annotation.ColorInt
 import android.support.annotation.StringRes
+import android.support.annotation.StyleableRes
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
@@ -40,6 +46,11 @@ open class CodeEditorView : ZoomLayout {
     lateinit var lineNumberView: TextView
 
     /**
+     * The divider between line numbers and text editor
+     */
+    lateinit var dividerView: View
+
+    /**
      * The actual EditText
      */
     lateinit var editTextView: CodeEditText
@@ -48,33 +59,23 @@ open class CodeEditorView : ZoomLayout {
     private var currentLineCount = -1
 
     constructor(context: Context) : super(context) {
-        initialize(null)
+        initialize(null, 0)
     }
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        initialize(attrs)
+        initialize(attrs, 0)
     }
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        initialize(attrs)
+        initialize(attrs, defStyleAttr)
     }
 
-    private fun initialize(attrs: AttributeSet?) {
-        readParameters(attrs)
-
+    private fun initialize(attrs: AttributeSet?, defStyleAttr: Int) {
         inflateViews(LayoutInflater.from(context))
+        readParameters(attrs, defStyleAttr)
+
         addView(contentLayout)
-
         setListeners()
-
-        editTextView
-                .setViewBackgroundWithoutResettingPadding(null)
-
-        editTextView
-                .post {
-                    editTextView
-                            .setSelection(0)
-                }
     }
 
     private fun View.setViewBackgroundWithoutResettingPadding(background: Drawable?) {
@@ -93,14 +94,91 @@ open class CodeEditorView : ZoomLayout {
     }
 
 
-    private fun readParameters(attrs: AttributeSet?) {
+    private fun readParameters(attrs: AttributeSet?, defStyleAttr: Int) {
+        val a = context
+                .obtainStyledAttributes(attrs, R.styleable.CodeEditorView, defStyleAttr, 0)
 
+        val lineNumberTextColor = a
+                .getColor(R.styleable.CodeEditorView_cev_lineNumbers_textColor, R.attr.cev_lineNumbers_textColor, android.R.attr.textColorPrimary)
+        lineNumberView
+                .setTextColor(lineNumberTextColor)
+
+        val lineNumberBackgroundColor = a
+                .getColor(R.styleable.CodeEditorView_cev_lineNumbers_backgroundColor, R.attr.cev_lineNumbers_backgroundColor, android.R.attr.windowBackground)
+        lineNumberView
+                .setBackgroundColor(lineNumberBackgroundColor)
+
+
+        val dividerEnabled = a
+                .getBoolean(R.styleable.CodeEditorView_cev_divider, true)
+        dividerView
+                .visibility = when (dividerEnabled) {
+            true -> View.VISIBLE
+            else -> View.GONE
+        }
+        val dividerColor = a
+                .getColor(R.styleable.CodeEditorView_cev_divider_color, R.attr.cev_divider_color, android.R.attr.textColorPrimary)
+        dividerView
+                .setBackgroundColor(dividerColor)
+
+        val editTextBackgroundColor = a
+                .getColor(R.styleable.CodeEditorView_cev_editor_backgroundColor, R.attr.cev_editor_backgroundColor, android.R.attr.windowBackground)
+        editTextView
+                .setBackgroundColor(editTextBackgroundColor)
+
+        a
+                .recycle()
     }
 
     private fun inflateViews(inflater: LayoutInflater) {
         contentLayout = inflater.inflate(R.layout.view_code_editor__inner_layout, null) as LinearLayout
+
         lineNumberView = contentLayout.findViewById(R.id.codeLinesView) as TextView
+
+        dividerView = contentLayout.findViewById(R.id.divider) as View
+
         editTextView = contentLayout.findViewById(R.id.codeEditText) as CodeEditText
+        editTextView
+                .setViewBackgroundWithoutResettingPadding(null)
+        editTextView
+                .post {
+                    editTextView
+                            .setSelection(0)
+                }
+    }
+
+    /**
+     * Get Color from Theme attribute
+     *
+     * @param context Activity context
+     * @param attr    Attribute ressource ID
+     *
+     * @return Color as Int
+     */
+    @ColorInt
+    private fun getThemeAttrColor(context: Context, @AttrRes attr: Int): Int? {
+        val typedValue = TypedValue()
+        if (context.theme.resolveAttribute(attr, typedValue, true)) {
+            if (typedValue.type >= TypedValue.TYPE_FIRST_INT && typedValue.type <= TypedValue.TYPE_LAST_INT) {
+                return typedValue
+                        .data
+            } else if (typedValue.type == TypedValue.TYPE_STRING) {
+                return ContextCompat
+                        .getColor(context, typedValue.resourceId)
+            }
+        }
+
+        return null
+    }
+
+    /**
+     * Get a color from this TypedArray or use the first default that is found
+     */
+    @ColorInt
+    private fun TypedArray.getColor(@StyleableRes styleableRes: Int, @AttrRes vararg attr: Int): Int {
+        return this
+                .getColor(styleableRes, attr.find { getThemeAttrColor(context, it) != null }
+                        ?: getThemeAttrColor(context, attr.last())!!)
     }
 
 
