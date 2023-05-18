6d80de9ae5a6bfab6e604b021acbc25cda989d11
Markus Ressel <mail@markusressel.de>
2023-05-18 22:36:21 +0200

    update targetSdk

--- build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
@@ -14,7 +14,7 @@ class AndroidTestConventionPlugin : Plugin<Project> {
 
             extensions.configure<TestExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 31
+                defaultConfig.targetSdk = 33
             }
         }
     }
