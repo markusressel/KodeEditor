6b17123cc69a3d50f37ac0e6f0dcb57097c5b858
Markus Ressel <mail@markusressel.de>
2024-07-14 15:46:40 +0200

    cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,15 +25,14 @@ dependencies {
     implementation(libs.kotlin.stdlib.jdk8)
 
     implementation(libs.androidx.appcompat)
-    implementation("com.google.android.material:material:1.9.0")
+    implementation(libs.material)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation(libs.androidx.activity.compose)
 
-    val fuelVersion = "2.3.1"
-    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
-    implementation("com.github.kittinunf.fuel:fuel-android:$fuelVersion")
+    implementation(libs.fuel)
+    implementation(libs.fuel.android)
 
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
